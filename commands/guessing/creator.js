9e46d3fe6f6f84c5fb7ec25e19dd0d739272cef6
ata <lushstars.nathan@gmail.com>
2024-06-16 09:12:46 +1000

    Create creator.js

---
+++ commands/guessing/creator.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const random = require("../../util/random.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('creatorguess')
+        .setDescription('Guess a randomly chosen creator!')
+        .addStringOption(option =>
+			option.setName('difficulty')
+				.setDescription('Choose a difficulty!')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Easy', value: 'Easy' },
+					{ name: 'Hard', value: 'Hard' },
+				)),
+    
+    async execute(interaction) {
+        var diff = interaction.options.getString('difficulty');
+        var randomC = random('creator');
+
+        do {
+            randomC = random('creator');
+        } while (randomC['difficulty'] != diff);
+
+        const embed = new EmbedBuilder()
+            .setColor(0xFFFF00)
+            .setDescription(`**Guess the Creator!**\nDifficulty: ${randomC['difficulty']}`)
+            .setImage(`https://mewo.lol/bot/images/creators/${randomC['file']}`);
+
+        const response = await interaction.reply({ embeds: [embed] })
+            .then(() => {
+                interaction.channel.awaitMessages({ time: 30_000, errors: ['time']})
+                    .then(collected => {
+                        if (collected.first().content.toLowerCase() === randomC['name'].toLowerCase()) {
+                            interaction.reply(`${collected.first().author} got it right!`);
+                            return;
+                        }
+                    })
+                    .catch(collected => {
+                        interaction.followUp("Times up!");
+                    });
+            });
+    }
+}
