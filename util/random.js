d27abad159b92bbdf063f0a68f53240208e59cde
ata <lushstars.nathan@gmail.com>
2024-06-16 09:26:27 +1000

    Create random.js

---
+++ util/random.js
@@ -0,0 +1,13 @@
+const { creators, levels } = require('../config.json');
+
+function random(type) {
+    if (type == "creator") {
+        const random = Math.floor(Math.random() * creators.length);
+        return creators[random];
+    } else if (type == "level") {
+        const random = Math.floor(Math.random() * levels.length);
+        return levels[random]; 
+    }
+}
+
+module.exports = random;
