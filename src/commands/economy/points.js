d3d9126dbae37be29e698c34dbfcf17682b4b7d8
at4pm <car0041@gssc.vic.edu.au>
2024-06-16 10:53:16 +1000

    Add points command and DB connection

---
+++ src/commands/economy/points.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const dbClient = new MongoClient(process.env.DBURI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+});
+  
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('balance')
+        .setDescription('Check your point balance!'),
+
+    async execute(interaction) {
+        let points = 0;
+        try {
+            await dbClient.connect();
+            const collection = dbClient.db().collection('users');
+            let user = await collection.findOne({ id: interaction.user.id });
+            if (!user) {
+                await collection.insertOne({
+                    id: interaction.user.id,
+                    points: 0
+                });
+                points = 0;
+            } else {
+                points = user.points;
+            }
+        } finally {
+            await dbClient.close();
+        }
+        
+        const balanceEmbed = new EmbedBuilder()
+            .setTitle('Balance')
+            .setColor(0xFFFF00)
+            .setDescription(`You have **${points}** points!`);
+        
+        await interaction.reply({ embeds: [balanceEmbed] });
+    }
+}
--- src/commands/guessing/creator.js
+++ src/commands/guessing/creator.js
@@ -23,19 +23,23 @@ module.exports = {
 			randomC = random('creator');
 		}
 
-		const embed = new EmbedBuilder()
+		const guessEmbed = new EmbedBuilder()
 			.setColor(0xFFFF00)
 			.setDescription(`**Guess the Creator!**\nDifficulty: ${randomC.difficulty}`)
 			.setImage(`https://mewo.lol/bot/images/creators/${randomC.file}`);
 
-		await interaction.reply({ embeds: [embed] });
+		await interaction.reply({ embeds: [guessEmbed] });
 
 		const filter = message => message.content.toLowerCase() === randomC.name.toLowerCase();
 		const collector = interaction.channel.createMessageCollector({ filter, time: 30_000 });
 
 		collector.on('collect', async message => {
 			collector.stop('guessed');
-			await message.reply('You got it right!');
+			const correctEmbed = new EmbedBuilder()
+				.setColor(0x00FF00)
+				.setTitle("**Correct!**")
+				.setDescription(`**${message.author}** correctly guessed **${randomC.name}**\nThey have gained **balls** coins!`);
+			await interaction.followUp({ embeds: [correctEmbed] });
 		});
 
 		collector.on('end', async (_collected, reason) => {
