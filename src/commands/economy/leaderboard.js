0b5542beb6e557ea2e3e3e1ab6afc3fdc3976d4b
at4pm <car0041@gssc.vic.edu.au>
2024-06-17 17:59:32 +1000

    Bug fixes (No one likes comments)

--- src/commands/economy/leaderboard.js
+++ src/commands/economy/leaderboard.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { getClient } = require('../../util/db.js');
+const { getClient, connectDB } = require('../../util/db.js');
 
 const dbClient = getClient();
 
@@ -9,17 +9,23 @@ module.exports = {
        .setDescription('View the points leaderboard!'),
     
     async execute(interaction) {
-        const leaderboard = dbClient.db("points").collection('users');
-        let users = await leaderboard.find().sort({ points: -1}).limit(10).toArray();
+        try {
+            await connectDB();
+            var leaderboard = dbClient.db("points").collection('users');
+
+            let users = await leaderboard.find().sort({ points: -1}).limit(10).toArray();
         
-        const embed = new EmbedBuilder()
-            .setTitle(":earth_asia: Global Leaderboard")
-            .setDescription('Points Leaderboard');
+            const embed = new EmbedBuilder()
+                .setTitle(":earth_asia: Global Leaderboard")
+                .setDescription('Points Leaderboard');
 
-        users.forEach((user, index) => {
-            embed.addFields({ name: `#${index + 1} ${interaction.guild.members.cache.get(user.id).user.tag}: ${user.points.toString()}`, value: "\u200B" });
-        });
+            users.forEach((user, index) => {
+                embed.addFields({ name: `#${index + 1}: ${user.points.toString()}`, value: " " });
+            });
 
-        await interaction.reply({ embeds: [embed] });
+            await interaction.reply({ embeds: [embed] });
+        } finally {
+            await dbClient.close();
+        }
     }
 };
