f8e2511165678f044f1a6d05cce9281e0e215daf
David Malchin <malchin459@gmail.com>
2024-06-22 02:38:58 +0300

    Refactor leaderboard command
    * Use schema util method `getUsername`.
    * Use `for await` to ensure valid sorting.

--- src/commands/economy/leaderboard.js
+++ src/commands/economy/leaderboard.js
@@ -1,36 +1,23 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { UserModel } = require('../../schema/user.js');
 
-const idToName = async (id) => {
-	const response = await fetch(`https://discord.com/api/v10/users/${id}`, {
-		headers: {
-			Authorization: `Bot ${process.env.TOKEN}`
-		},
-		method: 'GET',
-	});
-	if (!response.ok) return new Error(`API shit: ${response.status}`);
-	return await response.text();
-}
-
 module.exports = {
 	data: new SlashCommandBuilder()
 	   .setName('leaderboard')
 	   .setDescription('View the points leaderboard!'),
 	
 	async execute(interaction) {
-		let users = await UserModel.find().sort({ points: -1}).limit(10);
+		const users = await UserModel.find().sort({ points: -1 }).limit(10);
 	
 		const embed = new EmbedBuilder()
 			.setTitle(":earth_asia: Global Leaderboard")
 			.setDescription('Points Leaderboard');
 
-		await Promise.all(
-			users.map(async (user, index) => {
-				const data = await idToName(user.id);
-				const username = await JSON.parse(data).username;
-				embed.addFields({ name: `#${index + 1} ${username}: ${user.points.toString()}`, value: " " });
-			}),
-		);
+		for await (const [index, user] of users.entries()) {
+			const username = await user.getUsername(interaction.client);
+
+			embed.addFields({ name: `#${index + 1} ${username}: ${user.points.toString()}`, value: " " });
+		}
 
 		await interaction.reply({ embeds: [embed] });
 	}
