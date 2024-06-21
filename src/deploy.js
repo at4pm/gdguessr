32ae0f52a283c9f137a17264d05f02483b8458b5
David Malchin <malchin459@gmail.com>
2024-06-22 02:26:15 +0300

    Remove unnecessary try-catch in deploy script

--- src/deploy.js
+++ src/deploy.js
@@ -6,18 +6,15 @@ const commands = loadCommands(null, join(__dirname, 'commands'));
 const rest = new REST().setToken(process.env.TOKEN);
 
 async function deploy() {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-		const application = await rest.get(Routes.oauth2CurrentApplication());
-		const data = await rest.put(
-			Routes.applicationCommands(application.id),
-			{ body: commands },
-		);
+	console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		console.error(error);
-	}
+	const application = await rest.get(Routes.oauth2CurrentApplication());
+	const data = await rest.put(
+		Routes.applicationCommands(application.id),
+		{ body: commands },
+	);
+
+	console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 }
 
 deploy();
